64b9ae41386a8158f1f45c800b7dffc5698fdad5
tagliateller <tagliateller@users.noreply.github.com>
2023-09-11 12:45:37 +0200

    Update st.sh

--- st.sh
+++ st.sh
@@ -22,4 +22,5 @@ qemu-system-aarch64 \
   -m 1G -smp 4 -serial stdio \
   -kernel ./kernel8.img \
   -sd ./bullseye-arm64.img \ 
-  -append "rw earlyprintk loglevel=8 console=ttyAMA0,115100 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1"
+  -append "$(cat cmdline.txt)"
+#  -append "rw earlyprintk loglevel=8 console=ttyAMA0,115100 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1"
