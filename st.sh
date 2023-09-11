88e58348e33a978a7b2c97f856f7ad0120435753
tagliateller <tagliateller@users.noreply.github.com>
2023-09-11 18:09:20 +0200

    Update st.sh

--- st.sh
+++ st.sh
@@ -21,6 +21,6 @@ qemu-system-aarch64 \
   -dtb ./bcm2710-rpi-3-b.dtb \
   -m 1G -smp 4 -serial stdio \
   -kernel ./kernel8.img \
-  -sd ./bullseye-arm64.img \ 
+  -sd ./bullseye-arm64.img \
   -append "$(cat cmdline.txt)"
 #  -append "rw earlyprintk loglevel=8 console=ttyAMA0,115100 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1"
