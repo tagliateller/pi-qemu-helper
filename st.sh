a8138e2c1a7abf23f814a24f9d6543cd2c387405
tagliateller <tagliateller@users.noreply.github.com>
2023-09-11 11:58:18 +0200

    Update st.sh

--- st.sh
+++ st.sh
@@ -1,4 +1,25 @@
-
-
-
-qemu-system-aarch64 -machine raspi3b --cpu cortex-a72 -dtb ./bcm2710-rpi-3-b.dtb -m 1G -smp 4 -serial stdio -kernel ./kernel8.img -sd ./bullseye-arm64.img -append "rw earlyprintk loglevel=8 console=ttyAMA0,115100 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1"
+#"${qemu[$board]}" \
+#  -machine "$board" \
+#  -device usb-mouse \
+#  -device usb-kbd \
+#  -drive file="$img_file",if=sd \
+#  -netdev user,id=net0,hostfwd=tcp::8022-:22 \
+#  -device usb-net,netdev=net0 \
+#\
+#  -kernel "$kernel_file" \
+#  -append "$(cat $cmdline_file) $screen_args $EXTRA_ARGS" \
+#  -dtb "$dtb_file" \
+#\
+#  -no-reboot \
+#  -daemonize \
+#\
+#  "${qemu_extra[@]}"
+  
+qemu-system-aarch64 \
+  -machine raspi3b \
+  --cpu cortex-a72 \
+  -dtb ./bcm2710-rpi-3-b.dtb \
+  -m 1G -smp 4 -serial stdio \
+  -kernel ./kernel8.img \
+  -sd ./bullseye-arm64.img \ 
+  -append "rw earlyprintk loglevel=8 console=ttyAMA0,115100 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1"
