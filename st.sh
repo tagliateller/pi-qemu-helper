63e6fddb72fe5eadd0c0feb89a8f40dc70c3b5a9
tagliateller <tagliateller@users.noreply.github.com>
2023-09-13 17:59:12 +0200

    Update st.sh

--- st.sh
+++ st.sh
@@ -17,12 +17,25 @@
  
 # console=serial0,115200 console=tty1 root=PARTUUID=216b6d43-02 rootfstype=ext4 fsck.repair=yes rootwait quiet splash plymouth.ignore-serial-consoles
 
+#qemu-system-aarch64 \
+#  -machine raspi3b \
+#  --cpu cortex-a72 \
+#  -dtb ./bcm2710-rpi-3-b-plus.dtb \
+#  -kernel ./kernel8.img \
+#  -sd ./bullseye-arm64.img \
+#  -append "console=ttyAMA0,115100 console=tty1 root=PARTUUID=216b6d43-02 rootfstype=ext4 rschk.repair=yes rootwait quiet splash plymouth.ignore-serial-consoles bcm2708_fb.fbwidth=1024 bcm2708_fb.fbheight=768 dwc_otg.fiq_fsm_enable=0" \
+#  -no-reboot \
+#  -daemonize
+
+
 qemu-system-aarch64 \
-  -machine raspi3b \
-  --cpu cortex-a72 \
-  -dtb ./bcm2710-rpi-3-b-plus.dtb \
-  -kernel ./kernel8.img \
-  -sd ./bullseye-arm64.img \
-  -append "console=ttyAMA0,115100 console=tty1 root=PARTUUID=216b6d43-02 rootfstype=ext4 rschk.repair=yes rootwait quiet splash plymouth.ignore-serial-consoles bcm2708_fb.fbwidth=1024 bcm2708_fb.fbheight=768 dwc_otg.fiq_fsm_enable=0" \
-  -no-reboot \
-  -daemonize
+ -machine raspi3b \
+ --cpu cortex-a72 \
+ -dtb ./bcm2710-rpi-3-b.dtb \
+ -m 1G \
+ -smp 4 \
+ -serial stdio \
+ -kernel ./kernel8.img \
+ -sd ./bullseye-arm64.img \
+ -append "rw earlyprintk loglevel=8 console=ttyAMA0,115100 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1"
+ 
