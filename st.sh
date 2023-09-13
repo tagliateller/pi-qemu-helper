a63c6af0e06ba2bcb5ba6ec55f528914ecbb1583
tagliateller <tagliateller@users.noreply.github.com>
2023-09-13 20:38:06 +0200

    Update st.sh

--- st.sh
+++ st.sh
@@ -30,12 +30,13 @@
 
 qemu-system-aarch64 \
  -machine raspi3b \
- --cpu cortex-a72 \
+ -cpu cortex-a72 \
  -dtb ./bcm2710-rpi-3-b.dtb \
  -m 1G \
  -smp 4 \
  -serial stdio \
  -kernel ./kernel8.img \
  -sd ./bullseye-arm64.img \
+ -display gtk,gl=on
  -append "rw earlyprintk loglevel=8 console=ttyAMA0,115100 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1"
  
