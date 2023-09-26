c3507e7aec0ae7042400a91db3f11a63f802cfea
tagliateller <tagliateller@users.noreply.github.com>
2023-09-26 17:35:39 +0200

    Update st.sh

--- st.sh
+++ st.sh
@@ -36,7 +36,7 @@ qemu-system-aarch64 \
  -smp 4 \
  -serial stdio \
  -kernel ./kernel8.img \
- -sd ./bullseye-arm64.img \
- -display gtk,gl=on \
+ -sd ./bullseye-arm64-full.img \
+ -display gtk \
  -append "rw earlyprintk loglevel=8 console=ttyAMA0,115100 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1"
  
